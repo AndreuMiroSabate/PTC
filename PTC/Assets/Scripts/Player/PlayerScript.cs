53d9fc34aac004ec4dd374276be8cccf84055ad6
Jose Antonio Tur <turizquierdojoseantonio@gmail.com>
2024-12-03 11:55:52 +0100

    descomentada la cagalera 💩

--- PTC/Assets/Scripts/Player/PlayerScript.cs
+++ PTC/Assets/Scripts/Player/PlayerScript.cs
@@ -108,7 +108,7 @@ public class PlayerScript : MonoBehaviour
             transform.rotation *= deltaRotation;
         }
 
-        //updatePacket();
+        updatePacket();
     }
 
     void AimCannonAtMouse()
